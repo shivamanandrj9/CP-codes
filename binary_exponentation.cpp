5b8dedb17c26322e997d81099a3308c13c8f2b36
shivamanandrj9 <shivamanandrj9@gmail.com>
2021-10-07 20:58:54 +0530

    7

---
+++ binary_exponentation.cpp
@@ -0,0 +1,81 @@
+/*
+सरस्वति महाभागे विद्ये कमललोचने |
+विद्यारूपे विशालाक्षि विद्यां देहि नमोऽस्तु ते ||
+*/
+
+// https://www.youtube.com/watch?v=L-Wzglnm4dM
+
+#include<iostream>
+#include<algorithm>
+#include<numeric>
+#include<set>
+#include<queue>
+#include<climits>
+#include<deque>
+#include<map>
+#include<cmath>
+#include<vector>
+#include<cstring>
+#include<iomanip>
+
+using namespace std;
+
+#define int long long
+#define ff first
+#define ss second
+#define pb push_back
+#define vi vector<int>
+#define qi queue<int>
+#define mii map<int,int>
+#define pii pair<int,int>
+#define mip map<int,pair<int,int>>
+#define double long double
+#define FOR(i,k,n) for (int i = k; i < n; i++)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define double long double
+#define get(a) for(auto &i:a) cin>>i
+#define put(a) for(auto &i:a) cout<<i<<" "
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define endl "\n"
+#define sp <<" "<<
+#define P(x) cout<<x<<endl
+
+const int mod=3;
+const int INF = LLONG_MAX;
+
+int power(int a,int b)
+{
+    int result=1;
+    while(b)
+    {
+        if(b&1)
+        {
+            result*=a;
+            // result=result%mod;
+        }
+        a*=a; 
+        // a=a%mod;  
+        b/=2;
+    }
+    return result;
+}
+
+void solve()
+{
+    cout<<power(4,5);
+}
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+         solve();
+         cout<<endl;
+     }
+    return 0;
+}
